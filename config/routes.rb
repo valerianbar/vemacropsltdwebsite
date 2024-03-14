a240dda509aa7b22fe9e74f7c22e13d46ad97226
valerian barreau <valerianextra@gmail.com>
2024-03-14 09:54:16 +0300

    reprise du fichier master

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: "pages#home"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+  get "gallery", to: "pages#gallery"
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
