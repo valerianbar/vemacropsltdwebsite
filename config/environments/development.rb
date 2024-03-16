0b530d662eb7c6c0ad7be212c67097ca7e725521
valerian barreau <valerianextra@gmail.com>
2024-03-16 10:34:41 +0300

    host changement

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
   config.action_mailer.delivery_method = :smtp
   # host = "localhost:3000"
-  config.action_mailer.default_url_options = { host: "localhost", protocol: 3000 }
+  config.action_mailer.default_url_options = { host: "www.vemacrops.com", protocol: "http" }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
