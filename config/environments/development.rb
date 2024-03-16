89e81252db3653fdc7450f1f850ad7c047376090
valerian barreau <valerianextra@gmail.com>
2024-03-16 10:47:59 +0300

    host test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,8 +80,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
+    user_name: ENV.fetch("GMAIL_USERNAME"),
+    password: ENV.fetch("GMAIL_PASSWORD"),
     authentication: "plain",
     enable_starttls_auto: true
   }
